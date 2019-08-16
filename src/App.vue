3e33f10d8032e21946b5eb640509f3e736367dac
Sameer Ahmed <sameerahmed1472@gmail.com>
2019-08-16 23:50:26 +0500

    added /profile route and some dummy content

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,32 @@
 <template>
-<div class="app">
-<nav-bar></nav-bar>
-<router-view/>
-</div>
+  <div class="app">
+    <nav-bar></nav-bar>
+
+    <div class="rendered">
+      <div class="feed">
+        <cmp-feed></cmp-feed>
+      </div>
+      <div class="sideBar">
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 
   <script>
-  import nav from "./components/NavBar"
-    export default{
-components:{
-  navBar:nav
-}
-    }
+import feed from "./views/feed.vue";
+import nav from "./components/NavBar";
+export default {
+  components: {
+    navBar: nav,
+    cmpFeed: feed
+  }
+};
+</script>
 
-    </script>
+   <style lang="scss" scoped src="../public/css/main.scss">
+</style>
 
 
   
